917c3ab307d3c6d49b2f137d3190df58e7d99e67
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-01-23 01:47:07 +0900

    mod route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->get('/[{id}]', function ($id = null) use ($router) {
+    return "Hello, $id";
 });
