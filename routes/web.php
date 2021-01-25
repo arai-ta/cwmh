9bd4934016cf51356e711fa4fa1cbdd01ba67b90
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-01-26 02:30:00 +0900

    creating /callback

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
+use Illuminate\Log\Logger;
 use Illuminate\Support\Facades\Log;
+use League\OAuth2\Client\Grant\AuthorizationCode;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger as MonologLogger;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,4 +44,39 @@ $router->get('/start', function (\Illuminate\Http\Request $request) {
     $request->session()->put('state', $state);
 
     return redirect($url);
+});
+
+$router->get('/callback', function (\Illuminate\Http\Request $request) {
+    $state = $request->session()->get('state');
+
+    if ($state !== $request->input('state')) {
+        Log::error("invalid state error. session = {$state}, request = {$request->input('state')}");
+        return "invalid state error";
+    }
+
+    $logger = new MonologLogger('http_logging');
+    $logger->pushHandler(new StreamHandler('../storage/logs/http.log', MonologLogger::DEBUG));
+    $stack = HandlerStack::create();
+    $stack->push(
+        Middleware::log(
+            $logger,
+            new MessageFormatter('{uri} : {req_body} -> {res_body}')
+        )
+    );
+    $client = new GuzzleHttp\Client(['handler' => $stack]);
+
+    $provider = new ChatWork\OAuth2\Client\ChatWorkProvider(
+        env('OAUTH_CLIENT_ID'),
+        env('OAUTH_CLIENT_SECRET'),
+        url('/callback', [], true),
+        [
+            'httpClient' => $client
+        ]
+    );
+
+    $token = $provider->getAccessToken(new AuthorizationCode(), ['code' => $request->input('code')]);
+
+    dump($token);
+
+    return "OK";
 });
