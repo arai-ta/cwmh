845cd002e8ab1a800dd5e3b4d180a99ac10d93e1
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-05-03 20:04:18 +0900

    add create table

---
+++ database/migrations/2021_05_03_080811_create_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->integer('account_id')->unique();
+            $table->json('token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
