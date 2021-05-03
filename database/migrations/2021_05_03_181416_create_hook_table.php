0accedce5ff0f57811284a29b6a6ffc9925bda98
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-05-04 03:30:56 +0900

    add hook table

---
+++ database/migrations/2021_05_03_181416_create_hook_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hook', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->unique();
+            $table->string('key', 32);
+            $table->unsignedBigInteger('target_room_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hook');
+    }
+}
