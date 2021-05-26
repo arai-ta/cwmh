15b8befcc5b0f1708695532d6eb9445190c8717c
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-05-26 23:28:34 +0900

    apply bootstrap 5 starter template

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,13 +1,22 @@
-<html>
-    <head>
-        <title>
-@isset($title) {{ $title . ' - ' }} @endisset
-{{ config('app.name') }}
-        </title>
-    </head>
-    <body>
-        <h1>{{ $header ?? 'This is header' }}</h1>
-        <hr/>
-        {{ $slot }}
-    </body>
+<!doctype html>
+<html lang="ja">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>@isset($title) {{ $title . ' - ' }} @endisset {{ config('app.name') }}</title>
+</head>
+<body>
+<h1>{{ $header ?? 'This is header' }}</h1>
+
+<hr/>
+{{ $slot }}
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+</body>
 </html>
