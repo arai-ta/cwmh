f4a99beaee72650bfbb1d9c2b6bd825511aaf97f
ARAI Takashi <takashi.arai.nz@icloud.com>
2021-05-27 00:03:42 +0900

    copy from album template

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,9 +11,47 @@
     <title>@isset($title) {{ $title . ' - ' }} @endisset {{ config('app.name') }}</title>
 </head>
 <body>
-<h1>{{ $header ?? 'This is header' }}</h1>
 
-<hr/>
+<header>
+    <!--
+    <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-md-7 py-4">
+                    <h4 class="text-white">About Author</h4>
+                    <p class=""><img src="https://www.gravatar.com/avatar/04093049309e332c0f1c7e8f54c44deb?size=120"></p>
+                    <p class="text-muted">撮影者や撮影した背景など、アルバムに関する情報を追加しましょう。いくつかの文を書いておくと、友達が写真を選ぶ手助けになるかもしれません。また、写真は SNS や連絡先へとリンクしておきましょう。</p>
+                </div>
+                <div class="col-sm-4 offset-md-1 py-4">
+                    <h4 class="text-white">Contact</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="https://twitter.com/araitta" class="text-white">Twitter でフォローする</a></li>
+                        <li><a href="https://www.chatwork.com/arai-ta" class="text-white">Chatwork でつながる</a></li>
+                        <li><a href="https://github.com/arai-ta" class="text-white">GitHub で見る</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    -->
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container">
+            <a href="/" class="navbar-brand d-flex align-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-text-fill" viewBox="0 0 16 16">
+                    <path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM4.5 5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zm0 2.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zm0 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4z"/>
+                </svg>
+                &nbsp;
+                <strong>cwmh</strong>
+            </a>
+            <!-- ここにログアウトボタンを置く
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            -->
+        </div>
+    </div>
+</header>
+
 {{ $slot }}
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
